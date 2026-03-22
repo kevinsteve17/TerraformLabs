2a8cac950a89208b7092c17376b6945f1c3c4339
Ubuntu <azureuser@kube01.5aimf4w34elezcag4xoliquwfe.phxx.internal.cloudapp.net>
2026-03-22 15:10:19 +0000

    PracticaFinal - creando los files

---
+++ PracticaFinal/variables.tf
@@ -0,0 +1,44 @@
+variable "region" {
+  type = map(any)
+  default = {
+    "dev" = "us-east-1"
+    "prod" = "us-east-2"
+  }
+}
+
+variable "ami" {
+  type = map(any)
+  default = {
+    "dev" = "ami-033b95fb8079dc481"
+    "prod" = "ami-0b614a5d911900a9b"
+  }
+}
+
+variable "instancia" {
+  type = map(any)
+  default = {
+    "dev" = "t3a.nano"
+    "prod" = "t3a.micro"
+  }
+}
+
+variable "vpc_dir" {
+  type = map(any)
+  default = {
+    "dev" = "10.0.0.0/24"
+    "prod" = "10.0.0.0/24"
+  }
+}
+variable "subnet_cidr" {
+  type = map(any)
+  default = {
+    "dev" = "10.0.0.0/28"
+    "prod" = "10.0.0.0/28"
+  }
+}
+variable "tags" {
+  type = map(any)
+  default = {
+    "owner" = "Kevin Viquez Gomez"
+  }
+}
